078f8fe05deac53806c55f74535096724c0d3726
Krotov <krot.playstation@gmail.com>
2024-08-27 22:11:56 +0300

    Написаны тесты, заполнен файл README.md

---
+++ tests/test_product.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def test_product(product):
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.description == "256GB, Серый цвет, 200MP камера"
+    assert product.price == 180000.0
+    assert product.quantity == 5
