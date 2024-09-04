16a6561b2fa3614f35db8a13cadf64932952e566
Krotov <krot.playstation@gmail.com>
2024-09-04 18:49:16 +0300

    исправление тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,24 +14,10 @@ def product():
     )
 
 
-product_1 = ["55\" QLED 4K", "Фоновая подсветка", 123000.0, 7]
-
-
 @pytest.fixture
-def category():
+def category(product):
     return Category(
-        name="Телевизоры",
-        description="Современный телевизор, который позволяет наслаждаться просмотром",
-        products=[product_1]
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, но и получения функций для удобства жизни",
+        products=[product]
     )
-
-
-product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-category1 = Category(
-    "Смартфоны",
-    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-    [product1, product2, product3]
-)
--- tests/test_category.py
+++ tests/test_category.py
@@ -3,16 +3,16 @@ from src.product import Product
 from src.category import Category
 
 
-product_1 = ["55\" QLED 4K", "Фоновая подсветка", 123000.0, 7]
-
-
 def test_category(category):
-    assert category.name == "Телевизоры"
-    assert category.description == "Современный телевизор, который позволяет наслаждаться просмотром"
+    assert category.name == "Смартфоны"
+    assert category.description == "Смартфоны, как средство не только коммуникации, но и получения функций для удобства жизни"
 
     assert len(category.products_in_list) == 1
-    assert category.category_count == 2
+    assert category.category_count == 1
 
 
 def test_products_property(category):
-    assert '55" QLED 4K, 123000.0. Остаток: 7'
+    assert category.products == "Samsung Galaxy S23 Ultra, 180000.0. Остаток: 5\n"
+
+
+
