95011899a0b3fd86b408374387c877dc51cbaf63
Krotov <krot.playstation@gmail.com>
2024-09-04 16:40:57 +0300

    Добавлены геттеры и сеттеры для классов

--- src/product.py
+++ src/product.py
@@ -9,5 +9,24 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_dict):
+        cls.name = product_dict["name"]
+        cls.description = product_dict["description"]
+        cls.__price = product_dict["price"]
+        cls.quantity = product_dict["quantity"]
+        return cls(name=cls.name, description=cls.description, price=cls.__price, quantity=cls.quantity)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        self.__price = new_price
--- tests/test_category.py
+++ tests/test_category.py
@@ -1,4 +1,6 @@
 import pytest
+from src.product import Product
+from src.category import Category
 
 
 product_1 = ["55\" QLED 4K", "Фоновая подсветка", 123000.0, 7]
@@ -7,7 +9,10 @@ product_1 = ["55\" QLED 4K", "Фоновая подсветка", 123000.0, 7]
 def test_category(category):
     assert category.name == "Телевизоры"
     assert category.description == "Современный телевизор, который позволяет наслаждаться просмотром"
-    assert category.products == [product_1]
 
-    assert len(category.products) == 1
-    assert category.category_count == 1
+    assert len(category.products_in_list) == 1
+    assert category.category_count == 2
+
+
+def test_products_property(category):
+    assert '55" QLED 4K, 123000.0. Остаток: 7'
